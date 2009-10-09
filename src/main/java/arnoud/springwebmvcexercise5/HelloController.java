b5bc48ede93a04d4310ab0003ea789eb2d2654d9
ArnoudSanderse <ArnoudSanderse@hva.nl>
2009-10-09 10:38:35 +0200

    Cleanup Repository

--- src/main/java/arnoud/springwebmvcexercise5/HelloController.java
+++ src/main/java/arnoud/springwebmvcexercise5/HelloController.java
@@ -1,55 +1,25 @@
-package arnoud.springwebmvcexercise5;
+package arnoud.springwebmvcexercise4;
 
-import arnoud.model.*;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
  * @author arnoud
  */
 @Controller
-@Transactional
 public class HelloController {
-    @Autowired
-    public SessionFactory sessionFactory;
    
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String get(Model model) {
-        String message = "1+1=10";
-
-        Session session = sessionFactory.getCurrentSession();
-
-        try {
-            Student student = new Student(1, "Arnoud", "HvA", "IvI");
-            session.save(student);
-            message += "<br>- Saved data: " + student.toString();
-            session.flush();
-        } catch (Exception e) {
-            message += "<br>- Error saving data: " + e.getMessage();
+    public String get(Model model, HttpServletRequest request, HttpServletResponse response) {
+        String message = "";
+        if(request.getParameter("message") != null){
+            message = request.getParameter("message").toString();
         }
-
-        try {
-            Query q = session.createQuery("FROM Student");
-            if(q.list().size() > 0){
-                Student student = (Student) q.list().get(0);//new Student().getByName("Arnoud",session);
-                message += "<br>- Recieved data: " + student.toString();
-            }
-            else {
-                message += "<br>- No data Recieved";
-            }
-            session.flush();
-        } catch (Exception e) {
-            message += "<br>- Error retrieving data: " + e.getMessage();
-        }
-
         model.addAttribute("Message", message);
         return "hello";
     } 
